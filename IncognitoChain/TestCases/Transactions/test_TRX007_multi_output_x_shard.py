31e27e4075e171cbfed729660201577d004f1597
Hangntt <thanhhang11.04.96@gmail.com>
2020-12-11 09:54:19 +0700

    Update TRX007

--- IncognitoChain/TestCases/Transactions/test_TRX007_multi_output_x_shard.py
+++ IncognitoChain/TestCases/Transactions/test_TRX007_multi_output_x_shard.py
@@ -30,7 +30,7 @@ def setup_function():
         total_sent_amount += amount_to_be_received
         receiver_account_n_amount_dict[account] = amount_to_be_received
 
-    COIN_MASTER.top_him_up_prv_to_amount_if(total_sent_amount + 1000, total_sent_amount + 1000)
+    COIN_MASTER.top_him_up_prv_to_amount_if(total_sent_amount + 1000, total_sent_amount + 1000, sender_account)
 
 
 def test_send_prv_multi_output_privacy_x_shard_no_auto_fee():
