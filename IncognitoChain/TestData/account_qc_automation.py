20a8f06a4fbbb3eea6009d2606d2f6f76918fc1f
Yen Le <levu.daiyen@gmail.com>
2021-01-26 13:50:06 +0700

    add test bed and test data for QC automation

---
+++ IncognitoChain/TestData/account_qc_automation.py
@@ -0,0 +1,58 @@
+from IncognitoChain.Objects.AccountObject import Account
+
+account_list = [
+    Account(
+        '113aReGAe7JozDCrXLUA9WaAxYdeknqXigATa3wxkpt1VB88YZ8QcfNWFq8q6RLiaN9hwPaKfSDm69tJLVGBFENHTLUm8X6fPQT37DDnuxCt'),
+    Account(
+        '112uxQdveZpQNTqjpxDytFJtjr98zM8VhjyKn4m2RDbPeQBU54TBX9eNNJsbKG6bYbEQaCaVpNet3v9y7YpYNwZdib1f8BPNY8CP6dwNa7cw'),
+    Account(
+        '113F4VQ8cdJfPC7E3BG897Dhyuj1da6rEUZk1Ltef6p3BBA6VRdGaYErr8Gx9sacmL92HTPP25r1Z87sXBGTkBtwXc2GQ1hNM9FvLAiv2TcX'),
+    Account(
+        '1135dcfKsQC3cthNYud9QpBzZwHDpktbdw4cyKGAJFDcnH3xm6wfFtqnHuj7htwHvn1UmX6i55G2H2rK2oKZ7Y7TYDvhsJneLdhiTqhA2iLm'),
+    Account(
+        '113A4EtaaroFy38EmD8vBM9n4pYqtdh9VWiuGDPqsV5pJVeRLD9X5Cg9LEqc3akoVksYGgH9E45Z8AJmi37S4e5vjQ7oj6MwFAvLUwsx4V2D'),
+    Account(
+        '113iUBbh6j9QfWejhM27oU9f4jb2LYsRh6hZMMrbDmYB1cdo5FUvXVZaKMyFv6vSftughsvzaXhXA9zaPginffpMWsvJ7StGycYg45UVAPSW'),
+    Account(
+        '113FwzdTSA3fNz65GZ12q8n3UbeGmcnZbmXT1TQ6Nb7fdLKAvWgy37BhDeSGq5ygrVMLwsuYFWrZHKsGqR7JanYES44ouMGJqLEb8gjiEhp6'),
+    Account(
+        '1136vNE33gGfYA6EizeFJzayaNuCyjHZVF4yBaGjZZLqyTmcg7qy2xkQ9RKVpc82UTQDcMwCzNixkG1chFM1vkwEzWQv7YvHJG6RSvz8tKdy'),
+    Account(
+        '113STxbkyK8MMcfuhj1cVaq8ysBPyZ1NcGir3k2eMTSLvdj78YMMsfyY3db2r7zjjyJ37n99dSWFTppyMPdFM25HsirpAabA34kGMT24394y'),
+]
+
+committees = [
+    Account(
+        '112t8rnqijhT2AqiS8NBVgifb86sqjfwQwf4MHLMAxK3gr1mwxMaeUWQtR1MfxHscrKQ2MsyQMvJ3LEu49LEcZzTzoJCkCiewQ9p6wN5SrG1'),
+    Account(
+        '112t8rnud1R3of9rPkdKHWy8mQ5gMpXuBjLGhVrNurvHC93fF6qfiaEC8Nf7AHRbgrn1KF33akoNMNqUEUdSU7caXYvRL4uT58fhCuDV2Qs8'),
+    Account(
+        '112t8rnw7XyoehhKAUbNTqtVcda1eki89zrD2PfGMBKoYHvdE94ApWvXDtJvgotQohRot8yV52RZz2JjPtYGh4xsxb3gahn7RRWocEtW2Vei'),
+    Account(
+        '112t8ro1sHxz5xDkTs9i9VHA4cXVb5iqwCq2H2ffYYbGRh2wUHSHRRbnSQEMSnGiMvZAFLCccNzjZV9bSrphwGxxgtskVcauKNdgTEqA9bsf'),
+    Account(
+        '112t8ro424gNfJkKqDj25PjgWqCFTgG83TRERX1djUVr2wgJB6Lwk7NFi4pU8KxWSHsb4xK7UwPVYJ48FEGTzrB9jM58WfyvaJGCsT83jfNs'),
+    Account(
+        '112t8ro719sBgnX2GouVjLEZUvVwaepg8FkG35GtrFiFHeuE3y47PjXBbxHQdX1z87AAtEH5WCMZ8GUbhaZL3DbJuLqj7AAxGoc85damvB4J'),
+    Account(
+        '112t8rncy1vEiCMxvev5EkUQyfH9HLeManjS4kbcsSiMgp4FEiddsiMunhYL2pa8wciCAWxYtt9USgCv21fe2PkSxfnRkiq4AmDvJe33wtgV'),
+    Account(
+        '112t8rndMXjgDEGkuUmVedQxVYuZsQK5UvM9ZR1aZximBuNNJKpBn9j93MRqLBS17mHoCdLQNMmoYyuERZ1M3pRMG8SQj3NraHsG9eZPbbRK'),
+    Account(
+        '112t8rngznzWowvtXKyTnE9avawQGJCVgfJounHDT5nWucoVFv43TYu9PyjiGpPXXXQbCVEzxxCSfDmPNEBknK5B8n5qFiaddStg2M9pCYkZ'),
+    Account(
+        '112t8rnk4jduDzQGcmzKXr6r1F69TeQtHqDBCehDXPpwQTo7eDkEKFGMDGar46Jy4gmqSZDgwyUwpnxGkCnE2oEXmQ5FQpQJ4iMpDqLkgzwy'),
+    Account(
+        '112t8rnkSY1EtXtfZNGTKU6CFhfrdQ2YqLbLpfFEUGzVfoQeC6d47M5jWwv542aHJgEdtBKxmr2aikjxibL75rqGXEyKfUPXg1yp3xnCpL7D'),
+    Account(
+        '112t8rnmCktTnBnX866sSj5BzU33bUNZozJRes9xL7GPqSTQX9gsqG3qkiizsZuzV7BFs7CtpqNhcWfEMUZkkT7JnzknDB49jD2UBUemdnbK')]
+
+beacons = [
+    Account(
+        '112t8rncBDbGaFrAE7MZz14d2NPVWprXQuHHXCD2TgSV8USaDFZY3MihVWSqKjwy47sTQ6XvBgNYgdKH2iDVZruKQpRSB5JqxDAX6sjMoUT6'),
+    Account(
+        '112t8rnfXYskvWnHAXKs8dXLtactxRqpPTYJ6PzwkVHnF1begkenMviATTJVM6gVAgSdXsN5DEpTkLFPHtFVnS5RePi6aqTSth6dP4frcJUT'),
+    Account(
+        '112t8rngZ1rZ3eWHZucwf9vrpD1DNUAmrTTARSsptNDFrEoHv3QsDY3dZe8LXy3GeKXmeso8nUPsNwUM2qmZibQVXxGzstF4vZsYzJ83scFL'),
+    Account(
+        '112t8rnpXg6CLjvBg2ZiyMDgpgQoZuAjYGzbm6b2eXVSHUKjZUyb2LVJmJDPw4yNaP5M14DomzC514joTH3EVknRwnnGViWuH2HJuN6cpNhz')]
---
+++ IncognitoChain/TestData/account_qc_automation_2.py
@@ -0,0 +1,58 @@
+from IncognitoChain.Objects.AccountObject import Account
+
+account_list = [
+    Account(
+        '113fcJZNjF17yvb84P2XoX4to32NhZDAxgD7wxEwUCLvtNYifMjMV7iTu1RiYD5JctQArG3BhY9HxrkT4i4pQ9xDuN4jCnBmKn2JWeVVh9Ze'),
+    Account(
+        '113e7nr4P8UUBnVsnvD6bKygZFePvtEu2JRLsTv7wwu34RLuYhVeR5JDYW3ZoWWYE5wYwHKzov2sFh9DAmBdAeqh6sabmRMHePpJATqFeRmY'),
+    Account(
+        '112wHTYhALATcPwSwSJ31VU9csq9KRyC4QC9FoTGzTDrjLHYeDQ7WrSmXw34j8VSZA13LV7RLJUCNDvViM5xCH2QiuYzj6Gc9RS8TQfswyBf'),
+    Account(
+        '113C7TiC1NdPj3D8C2zy2isNh9X4xkNJMkQ4jPW3xWCXhSRZ6cpthfw4Srs1gvhxE6ovjSQPoAUicC1v5dpgXW72NqZV4XAnTe18Kn6cGEer'),
+    Account(
+        '1139W8HYbwdAThSfYMuMeByQTsKbyHSPHXYQxgxM8ThibR6eo9Y34zacXnkLmGMgCcisJGoybXHizd4ozsbEg5LXroV5ekd3Bt7fe2FeMfRe'),
+    Account(
+        '112wE5Cdv1UJt2kenRPgh9gu3MFAU9nA1s6FL6r5mtzNrwhXF4UqyTPD6auqhmQreea6YHUXK7VWqZ6boY83rtoLBpehTiLePhfTCqgjHF52'),
+    Account(
+        '113DHLrnmTpKNiNYtSJ9cMTjLfjcKVix3DvrbkV37tqq1xoHrVYUZK6n5dqcBvgs1TckNR8DME5T3kaUnxNHcrTe2PcyQa6u5q27DyTkAQRP'),
+    Account(
+        '112wDAyAV1PQdtudgRQi9fwsrXRBJ5Av5iEgo6aDB3m44jajbAzSy3gK2feHrETbnoxyowZhb13pmNrgPTe74NEz7o9eU7UMV1Lpr4kaXc4W'),
+    Account(
+        '113RZxAAFrQMxqLK341L4JLPuM2t34FgRWQe5QzUVqz8edEpMwFtVj3PEGYLU4mS3bkK6fCvL6DpPyZp1MvAc3VQBRpZY1Uqy8cX3BHajjXE'),
+]
+
+committees = [
+    Account(
+        '112t8rnqijhT2AqiS8NBVgifb86sqjfwQwf4MHLMAxK3gr1mwxMaeUWQtR1MfxHscrKQ2MsyQMvJ3LEu49LEcZzTzoJCkCiewQ9p6wN5SrG1'),
+    Account(
+        '112t8rnud1R3of9rPkdKHWy8mQ5gMpXuBjLGhVrNurvHC93fF6qfiaEC8Nf7AHRbgrn1KF33akoNMNqUEUdSU7caXYvRL4uT58fhCuDV2Qs8'),
+    Account(
+        '112t8rnw7XyoehhKAUbNTqtVcda1eki89zrD2PfGMBKoYHvdE94ApWvXDtJvgotQohRot8yV52RZz2JjPtYGh4xsxb3gahn7RRWocEtW2Vei'),
+    Account(
+        '112t8ro1sHxz5xDkTs9i9VHA4cXVb5iqwCq2H2ffYYbGRh2wUHSHRRbnSQEMSnGiMvZAFLCccNzjZV9bSrphwGxxgtskVcauKNdgTEqA9bsf'),
+    Account(
+        '112t8ro424gNfJkKqDj25PjgWqCFTgG83TRERX1djUVr2wgJB6Lwk7NFi4pU8KxWSHsb4xK7UwPVYJ48FEGTzrB9jM58WfyvaJGCsT83jfNs'),
+    Account(
+        '112t8ro719sBgnX2GouVjLEZUvVwaepg8FkG35GtrFiFHeuE3y47PjXBbxHQdX1z87AAtEH5WCMZ8GUbhaZL3DbJuLqj7AAxGoc85damvB4J'),
+    Account(
+        '112t8rncy1vEiCMxvev5EkUQyfH9HLeManjS4kbcsSiMgp4FEiddsiMunhYL2pa8wciCAWxYtt9USgCv21fe2PkSxfnRkiq4AmDvJe33wtgV'),
+    Account(
+        '112t8rndMXjgDEGkuUmVedQxVYuZsQK5UvM9ZR1aZximBuNNJKpBn9j93MRqLBS17mHoCdLQNMmoYyuERZ1M3pRMG8SQj3NraHsG9eZPbbRK'),
+    Account(
+        '112t8rngznzWowvtXKyTnE9avawQGJCVgfJounHDT5nWucoVFv43TYu9PyjiGpPXXXQbCVEzxxCSfDmPNEBknK5B8n5qFiaddStg2M9pCYkZ'),
+    Account(
+        '112t8rnk4jduDzQGcmzKXr6r1F69TeQtHqDBCehDXPpwQTo7eDkEKFGMDGar46Jy4gmqSZDgwyUwpnxGkCnE2oEXmQ5FQpQJ4iMpDqLkgzwy'),
+    Account(
+        '112t8rnkSY1EtXtfZNGTKU6CFhfrdQ2YqLbLpfFEUGzVfoQeC6d47M5jWwv542aHJgEdtBKxmr2aikjxibL75rqGXEyKfUPXg1yp3xnCpL7D'),
+    Account(
+        '112t8rnmCktTnBnX866sSj5BzU33bUNZozJRes9xL7GPqSTQX9gsqG3qkiizsZuzV7BFs7CtpqNhcWfEMUZkkT7JnzknDB49jD2UBUemdnbK')]
+
+beacons = [
+    Account(
+        '112t8rncBDbGaFrAE7MZz14d2NPVWprXQuHHXCD2TgSV8USaDFZY3MihVWSqKjwy47sTQ6XvBgNYgdKH2iDVZruKQpRSB5JqxDAX6sjMoUT6'),
+    Account(
+        '112t8rnfXYskvWnHAXKs8dXLtactxRqpPTYJ6PzwkVHnF1begkenMviATTJVM6gVAgSdXsN5DEpTkLFPHtFVnS5RePi6aqTSth6dP4frcJUT'),
+    Account(
+        '112t8rngZ1rZ3eWHZucwf9vrpD1DNUAmrTTARSsptNDFrEoHv3QsDY3dZe8LXy3GeKXmeso8nUPsNwUM2qmZibQVXxGzstF4vZsYzJ83scFL'),
+    Account(
+        '112t8rnpXg6CLjvBg2ZiyMDgpgQoZuAjYGzbm6b2eXVSHUKjZUyb2LVJmJDPw4yNaP5M14DomzC514joTH3EVknRwnnGViWuH2HJuN6cpNhz')]
