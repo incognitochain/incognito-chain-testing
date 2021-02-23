28be826849825e3cd14fbbc3b79a0f557a1916c8
Yen Le <levu.daiyen@gmail.com>
2021-02-23 15:01:28 +0700

    fix test runner testcases select

---
+++ run-jenkin-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# python=./venv/bin/python3
+python=python3
+
+test_bed=$1
+test_data=$2
+test_folder_name=$3
+html_report="reports/${test_folder_name}/$(date '+%Y.%m.%d-%H.%M.%S')-${test_bed}-${test_data}.html"
+xoption=""
+
+if [ "$test_bed" != "-" ]; then
+  echo " !!! Using test bed: $test_bed"
+  xoption+="-XtestBed=$test_bed "
+fi
+if [ "$test_data" != "-" ]; then
+  echo " !!! Using test data: $test_data"
+  xoption+="-XtestData=$test_data "
+fi
+
+set -x
+$python $xoption -m pytest \
+  --show-capture=stderr --capture=tee-sys -v --html="$html_report" --self-contained-html $4
