15c6c11e880563d0e4e1155958371139cd4a463d
Rocky <thach@incognito.org>
2021-11-11 08:49:44 +0700

    add pdex3 bulk swap verify receive test

--- Objects/__init__.py
+++ Objects/__init__.py
@@ -31,17 +31,17 @@ class BlockChainInfoBaseClass(ABC):
     def is_none(self):
         return bool(self.dict_data)
 
-    def pretty_format(self):
+    def pretty(self):
         return json.dumps(self.dict_data, indent=3)
 
     def pretty_print(self):
-        print(self.pretty_format())
+        print(self.pretty())
 
     def clone(self):
         return self.__deepcopy__()
 
     def __str__(self):
-        return self.pretty_format()
+        return self.pretty()
 
     def _1_item_dict_key(self):
         return list(self.dict_data.keys())[0]
