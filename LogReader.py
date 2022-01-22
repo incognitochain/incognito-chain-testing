f1d0184d301498f42e65a08050c29837cc3882a7
Rocky <thach@incognito.org>
2022-01-22 09:20:42 +0700

    LogReader.py filter log by log level

---
+++ LogReader.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import re
+import sys
+
+try:
+    log_file = sys.argv[1]
+except IndexError:
+    print("Please specify a log file")
+    exit()
+try:
+    log_level = sys.argv[2]
+except IndexError:
+    print("Log level is not specified, use INFO instead")
+    log_level = "INFO"
+
+LOG_LVL = {'CRITICAL': 50,
+           'FATAL': 50,
+           'ERROR': 40,
+           'WARNING': 30,
+           'WARN': 30,
+           'INFO': 20,
+           'DEBUG': 10,
+           'NOTSET': 0}
+
+
+def get_log_level_of(line):
+    regex = re.compile(r'^\d{2}:\d{2}:\d{2} ([A-Z]+)')
+    match = re.match(regex, line)
+    return LOG_LVL[match.group(1)] if match else -1
+
+
+with open(log_file, "r") as file:
+    line = 1
+    still_print = False
+    while line:
+        line = file.readline().strip("\n")
+        lvl = get_log_level_of(line)
+        if lvl >= LOG_LVL[log_level]:
+            still_print = True
+            print(line) if still_print else None
+        elif lvl == -1:
+            print(line) if still_print else None
+        else:
+            still_print = False
